b11764f6bc3ae7e51c9f9d79a0c47d48642619cd
DinoSirovica <dino14sirovica@gmail.com>
2022-12-18 22:15:01 +0100

    First push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    RegistrationComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
