62000e8bf032a5fbeb7effa1443fd813cd2475e2
DinoSirovica <dino14sirovica@gmail.com>
2022-12-20 22:30:43 +0100

    Added connection to FireBase to the app

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {PostClass} from "../post-class";
+import {HttpClient} from "@angular/common/http";
+import {map} from "rxjs";
 
 @Component({
   selector: 'app-main',
@@ -6,39 +9,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
-  listOfPosts: { id:number; username: string; comment: string; isEditing:boolean, date: Date}[] = [
-    {
-      id:1, username: "test1", comment:"comment1", isEditing:false, date: new Date()
-    },
-    {
-      id:2, username: "test1", comment:"comment1", isEditing:false, date: new Date()
-    }
-  ];
+  // listOfPosts: { id:number; username: string; comment: string; isEditing:boolean, date: Date}[] = [
+  //   {
+  //     id:1, username: "test1", comment:"comment1", isEditing:false, date: new Date()
+  //   },
+  //   {
+  //     id:2, username: "test1", comment:"comment1", isEditing:false, date: new Date()
+  //   }
+  // ];
 
-  constructor() {}
+  listOfPosts : PostClass[] = [];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://njs-lab5-default-rtdb.europe-west1.firebasedatabase.app/posts.json').pipe(
+      map(
+        res => {
+          console.log(res)
+          const postsArray = [];
+          let key: keyof typeof res;
+          for (key in res) {
+            postsArray.push({...res[key], id: key})
+          }
+          return postsArray;
+        }
+      )
+    ).subscribe((data: any) => {
+      console.log(data)
+      for(let i = 0; i < data.length; i++) {
+        this.listOfPosts.push({
+          id: data[i].id,
+          userId: data[i].userId,
+          comment: data[i].comment,
+          isEditing: false,
+          date: data[i].timestamp
+        })
+      }
+    })
+  }
+
   addComment = false
   comment = ""
   username = ""
-  id=3;
+  id="";
   isEditing = false
-  idToEdit = 0
+  idToEdit = "0"
 
   addPost() {
-    this.id++;
     console.log(this.username)
     console.log(this.comment)
-    this.listOfPosts.push({id: this.id, username: this.username, comment: this.comment, isEditing: false, date: new Date()})
+    this.listOfPosts.push({id: this.id, userId: this.username, comment: this.comment, isEditing: false, date: new Date().toString()})
     this.addComment = false
     this.comment = ""
     this.username = ""
   }
 
-  editPost(id:number, isEditing:boolean) {
+  editPost(id:string, isEditing:boolean) {
     if(isEditing) {
       this.idToEdit = id
     }
     else {
-      this.idToEdit = 0
+      this.idToEdit = "0"
     }
     this.listOfPosts.map((post) => {
       if (post.id === id) {
@@ -47,11 +78,11 @@ export class MainComponent {
     })
   }
 
-  getPostById(id:number){
+  getPostById(id:string){
     return this.listOfPosts.find(e => e.id==id)
   }
 
-  deletePost(id:number) {
+  deletePost(id:string) {
     this.listOfPosts = this.listOfPosts.filter((post) => post.id !== id)
   }
 }
